2d88281f97649f0b3c779dfebcf223e535c677b0
Pedro Henrique <pedrobragaom@gmail.com>
2019-11-07 17:51:52 -0300

    Product list - star component

--- Angular/Acme/src/app/app.module.ts
+++ Angular/Acme/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import {ProductListComponent} from './product/product-list.component';
 import {FormsModule} from '@angular/forms';
 import {changeToSapce} from './shared/change-to-spaces.pipe';
+import {StarComponent} from './shared/star.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
-    changeToSapce
+    changeToSapce,
+    StarComponent
   ],
   imports: [
     BrowserModule,
---
+++ Angular/Acme/src/app/shared/star.component.ts
@@ -0,0 +1,11 @@
+import {Component, OnChanges} from '@angular/core';
+
+@Component({
+  selector: 'pm-star',
+  templateUrl: 'star.component.html',
+  styleUrls: ['star.component.css']
+})
+export class StarComponent implements OnChanges {
+  ngOnChanges(changes: SimpleChanges): void {
+  }
+}
