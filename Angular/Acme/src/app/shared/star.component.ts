708f35f260ac4490bb9b73a4320d655be352348c
Pedro Henrique <pedrobragaom@gmail.com>
2019-11-25 17:54:03 -0300

    Product list - nested component

--- Angular/Acme/src/app/shared/star.component.ts
+++ Angular/Acme/src/app/shared/star.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnChanges} from '@angular/core';
+import {Component, EventEmitter, Input, OnChanges, Output} from '@angular/core';
 
 @Component({
   selector: 'pm-star',
@@ -7,8 +7,12 @@ import {Component, Input, OnChanges} from '@angular/core';
 })
 export class StarComponent implements OnChanges {
   @Input() rating: number;
+  @Output() starClicked = new EventEmitter<string>();
   starWidth: number;
   ngOnChanges(): void {
     this.starWidth = this.rating * 75 / 5;
+  }
+  onClickStar(): void {
+    this.starClicked.emit('The rating ${this.rating} was clicked');
   }
 }
